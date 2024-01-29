b3cd562b0c5f3867326088260705996e66e0328d
CodingRules123 <157979538+CodeMaster589@users.noreply.github.com>
2024-01-29 17:30:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 2; index++) {
+        music.play(music.stringPlayable("G B A G C5 B A B ", 129), music.PlaybackMode.InBackground)
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(500)
+    }
+    basic.showArrow(input.temperature())
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index <= 2; index++) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+        basic.showNumber(3 - index)
+    }
+    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    basic.showString("GO!")
+    steps = 0
+    for (let index = 0; index < 100000000000000000000; index++) {
+        basic.showNumber(steps)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    steps += 1
+})
+let steps = 0
+for (let index = 0; index < 2; index++) {
+    music.play(music.stringPlayable("C D E F G A B - ", 120), music.PlaybackMode.InBackground)
+    basic.showString("WELCOME")
+}
