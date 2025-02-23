b2541b7dd6860ca9a5d6cff7260a77226b1de07d
CodingRules123 <157979538+CodeMaster589@users.noreply.github.com>
2025-02-23 10:37:11 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def on_button_pressed_a():
-    for index in range(2):
-        music.play(music.string_playable("G B A G C5 B A B ", 129),
+    for index in range(1):
+        music.play(music.string_playable("G B A G C5 B A B ", 120),
             music.PlaybackMode.IN_BACKGROUND)
         basic.show_icon(IconNames.HEART)
         basic.pause(500)
@@ -11,25 +11,27 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global steps
-    for index2 in range(3):
+    for countdown_numbers in range(3):
         music.play(music.tone_playable(262, music.beat(BeatFraction.QUARTER)),
             music.PlaybackMode.UNTIL_DONE)
-        basic.show_number(3 - index2)
-    music.play(music.tone_playable(392, music.beat(BeatFraction.WHOLE)),
-        music.PlaybackMode.UNTIL_DONE)
+        basic.show_number(3 - countdown_numbers)
+    music.play(music.tone_playable(392, music.beat(BeatFraction.DOUBLE)),
+        music.PlaybackMode.IN_BACKGROUND)
     basic.show_string("GO!")
     steps = 0
-    for index3 in range(100000000000000000000):
+    for index2 in range(1000000000):
         basic.show_number(steps)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_shake():
     global steps
     steps += 1
+    led.stop_animation()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 steps = 0
-for index4 in range(2):
-    music.play(music.string_playable("C D E F G A B - ", 120),
+music.set_built_in_speaker_enabled(True)
+for index3 in range(1):
+    music.play(music.string_playable("G B A G C5 B A B ", 120),
         music.PlaybackMode.IN_BACKGROUND)
-    basic.show_string("WELCOME")
+    basic.show_string("Hello")
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     for (let index = 0; index < 1; index++) {
         music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
         basic.showIcon(IconNames.Heart)
@@ -8,25 +8,27 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showArrow(input.temperature())
 })
-input.onButtonPressed(Button.B, function () {
-    for (let countdown_numbers = 0; countdown_numbers <= 2; countdown_numbers++) {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    for (let countdown_numbers = 0; countdown_numbers < 3; countdown_numbers++) {
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
         basic.showNumber(3 - countdown_numbers)
     }
     music.play(music.tonePlayable(392, music.beat(BeatFraction.Double)), music.PlaybackMode.InBackground)
     basic.showString("GO!")
     steps = 0
-    for (let index = 0; index < 1000000000; index++) {
+    for (let index2 = 0; index2 < 1000000000; index2++) {
         basic.showNumber(steps)
     }
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
     steps += 1
     led.stopAnimation()
 })
 let steps = 0
 music.setBuiltInSpeakerEnabled(true)
-for (let index = 0; index < 1; index++) {
+for (let index3 = 0; index3 < 1; index3++) {
     music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
     basic.showString("Hello")
 }
