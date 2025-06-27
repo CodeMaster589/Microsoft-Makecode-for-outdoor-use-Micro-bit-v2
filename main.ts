3c1adcfaaf119ec4ec34b8959273191aa4ba2a6b
CodingRules123 <157979538+CodeMaster589@users.noreply.github.com>
2025-06-27 19:30:25 -0400

    Tested the code with my micro:bit V2.21, and found some issues. I have solved the issued and changed many things.

--- main.ts
+++ main.ts
@@ -1,34 +1,34 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 1; index++) {
         music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
         basic.showIcon(IconNames.Heart)
         basic.pause(500)
         basic.showIcon(IconNames.SmallHeart)
         basic.pause(500)
+        basic.showNumber(input.temperature())
     }
-    basic.showArrow(input.temperature())
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
-    for (let countdown_numbers = 0; countdown_numbers < 3; countdown_numbers++) {
+input.onButtonPressed(Button.B, function () {
+    for (let countdown_numbers = 0; countdown_numbers <= 2; countdown_numbers++) {
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
         basic.showNumber(3 - countdown_numbers)
     }
     music.play(music.tonePlayable(392, music.beat(BeatFraction.Double)), music.PlaybackMode.InBackground)
     basic.showString("GO!")
     steps = 0
-    for (let index2 = 0; index2 < 1000000000; index2++) {
-        basic.showNumber(steps)
-    }
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    
+input.onGesture(Gesture.Shake, function () {
     steps += 1
-    led.stopAnimation()
+    basic.showNumber(steps)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    record.setMicGain(record.AudioLevels.Medium)
+    record.startRecording(record.BlockingState.Blocking)
+    if (true) {
+        record.playAudio(record.BlockingState.Blocking)
+    }
 })
 let steps = 0
 music.setBuiltInSpeakerEnabled(true)
-for (let index3 = 0; index3 < 1; index3++) {
-    music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
-    basic.showString("Hello")
-}
+music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.InBackground)
+basic.showString("Hello")
